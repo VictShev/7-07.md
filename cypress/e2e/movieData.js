58b5385a7171544eb554cc6ec3d2229cbd306c07
victshev <victshev259@gmail.com>
2024-03-28 18:53:02 +0400

    Initial commit

---
+++ cypress/e2e/movieData.js
@@ -0,0 +1,13 @@
+const movieData = {
+
+"title": "Драйв",
+"director": "Николас Виндинг Рефн",
+"genre": "Криминал, драма, триллер",
+"releaseDate": "2019-04-26",
+"duration": "100 минут",
+"description": "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+"hall": "Hall 1",
+"time": "2024-04-01T19:00:00"
+};
+
+  module.exports = movieData;
